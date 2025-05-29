5dd7ad9dc32e7cc6b64c959f1a6d063d40f9380b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-29 15:11:51 -0600

    Initial Commit

---
+++ Reto 13/Reto_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Reto_13App.swift
+//  Reto 13
+//
+//  Created by Benjamin Rojo on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Reto_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
